fea72e26d8d201ef4a2dd4677041497ea1ce98a0
METR0109 <110671168+METR0109@users.noreply.github.com>
2024-04-07 14:13:12 +0300

    sub

--- js/tast-1.js
+++ js/tast-1.js
@@ -1,14 +1,11 @@
-function makeTransaction(quantity, pricePerDroid, customerCredits) {
-  const totalPrice = quantity * pricePerDroid;
-  if (totalPrice > customerCredits) {
-    return 'Insufficient funds!';
-  } else {
-    return `You ordered ${quantity} droids worth ${totalPrice} credits!`;
-  }
-}
+// ! Масиви і функції
+//TODO:=========task-01=================
+// Створіть масив styles з елементами «Джаз» та «Блюз».
+// Додайте "Рок-н-рол" в кінець.
+// Замініть значення "Блюз" на "Класика".
+// Видалить перший елемент масиву і виведіть його в консоль.
+// Вставте «Реп» і «Реггі» на початок масиву.
 
-console.log(makeTransaction(5, 3000, 23000)); // "You ordered 5 droids worth 15000 credits!"
-console.log(makeTransaction(3, 1000, 15000)); // "You ordered 3 droids worth 3000 credits!"
-console.log(makeTransaction(10, 5000, 8000)); // "Insufficient funds!"
-console.log(makeTransaction(8, 2000, 10000)); // "Insufficient funds!"
-console.log(makeTransaction(10, 500, 5000)); // "You ordered 10 droids worth 5000 credits!"
+const styles = ['Jazz', 'Bluz'];
+
+console.log(styles);
